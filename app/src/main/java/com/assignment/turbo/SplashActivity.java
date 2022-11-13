d661366d0be5b16e61797fa12fbe6cb5f45bac25
Sultan <sultan.ruet113054@gmail.com>
2022-11-13 11:22:44 +0600

    splashscreen done

---
+++ app/src/main/java/com/assignment/turbo/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.assignment.turbo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        handler=new Handler();
+        handler.postDelayed(() -> {
+            Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+            startActivity(intent);
+        },2000);
+
+    }
+}
